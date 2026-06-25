7292b17d1213b6a06851ac08e5490ba756bd8e77
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-06-25 21:24:09 +0530

    0146-LRU-CACHE Stats: Time: 95 ms (36.94%), Space: 186.7 MB (14.1%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,73 @@
+class LRUCache {
+public:
+    class Node {
+    public:
+       int key;
+       int val;
+       Node* next;
+       Node* prev;
+       // Constructor
+       Node(int _key,int _val){
+            key = _key;
+            val = _val;
+       }
+    };
+
+    Node* head = new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
+
+    int cap;
+    unordered_map<int,Node*> m;
+
+
+    LRUCache(int capacity) {
+        cap = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    void addNode( Node* newNode ){
+        Node* temp = head->next;
+        newNode->next = temp;
+        newNode->prev = head;
+        head->next = newNode;
+        temp->prev = newNode;
+    }
+
+    void deletNode( Node* delNode){
+        Node* delprev = delNode->prev;
+        Node* delnext = delNode->next;
+        delnext->prev = delprev;
+        delprev->next = delnext;
+
+    }
+    
+    int get(int key_) {
+        if(m.find(key_) != m.end()){
+            Node* resNode = m[key_];
+            m.erase(key_);
+            deletNode(resNode);
+            addNode(resNode);
+            m[key_] = head->next;
+            return resNode->val;
+        }
+        return -1;
+        
+    }
+    
+    void put(int key_, int value) {
+        if(m.find(key_) != m.end()){
+            Node* existingNode = m[key_];
+            m.erase(key_);
+            deletNode(existingNode);
+        }
+        if(m.size() == cap ){
+            m.erase(tail->prev->key);
+            deletNode(tail->prev);
+        }
+
+        addNode(new Node(key_,value));
+        m[key_] = head->next;
+        
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # LeetCode
 A collection of LeetCode questions to ace the coding interview! - Created using [LeetHub 2.0](https://github.com/maitreya2954/LeetHub-2.0-Firefox)
+
+<!---LeetCode Topics Start-->
+# LeetCode Topics
+## Hash Table
+|  |
+| ------- |
+| [0146-lru-cache](https://github.com/Harshal006-rgb/LeetCode/tree/master/0146-lru-cache) |
+## Linked List
+|  |
+| ------- |
+| [0146-lru-cache](https://github.com/Harshal006-rgb/LeetCode/tree/master/0146-lru-cache) |
+## Design
+|  |
+| ------- |
+| [0146-lru-cache](https://github.com/Harshal006-rgb/LeetCode/tree/master/0146-lru-cache) |
+## Doubly-Linked List
+|  |
+| ------- |
+| [0146-lru-cache](https://github.com/Harshal006-rgb/LeetCode/tree/master/0146-lru-cache) |
+<!---LeetCode Topics End-->
