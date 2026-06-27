f6355b885759b6f686fff60c304ec8ecfe57b350
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-06-28 02:02:32 +0530

    0090-SUBSETS-II Stats: Time: 0 ms (100%), Space: 10.4 MB (67.13%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -4,21 +4,34 @@ public:
         vector<vector<int>> res;
         vector<int> subset;
         sort(nums.begin(), nums.end());
-        backtrack(0, nums, subset, res);
+        rec(0, nums, subset, res);
         return res;        
     }
 
-private:
-    void backtrack(int i, vector<int>& nums, vector<int>& subset, vector<vector<int>>& res) {
-        if (i == nums.size()) {
-            res.push_back(subset);
-            return;
-        }
+    void rec(int idx, vector<int>& nums, vector<int>& subset, vector<vector<int>>& res) {
 
-        subset.push_back(nums[i]);
-        backtrack(i + 1, nums, subset, res);
-        subset.pop_back();
-        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) i++;
-        backtrack(i + 1, nums, subset, res);
+        //Method-1
+
+        res.push_back(subset);
+
+        for( int i = idx ; i < nums.size() ; i++ ) {
+            if(i>idx && nums[i] == nums[i-1]) continue;
+            subset.push_back(nums[i]);
+            rec(i+1,nums,subset,res);
+            subset.pop_back();
+        }
+        
+        // Method - 2
+        // if (i == nums.size()) {
+        //     res.push_back(subset);
+        //     return;
+        // }
+        // subset.push_back(nums[i]);
+        // rec(i + 1, nums, subset, res);
+        // subset.pop_back();
+        // while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+        //     i++;
+        // }
+        // rec(i + 1, nums, subset, res);
     }    
 };
