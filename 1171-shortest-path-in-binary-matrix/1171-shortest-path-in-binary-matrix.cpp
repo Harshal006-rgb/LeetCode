d6ce48ebc83c21e323fbd6d27c255d3315a0cdc3
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-07-07 04:02:35 +0530

    1171-SHORTEST-PATH-IN-BINARY-MATRIX Stats: Time: 41 ms (18.84%), Space: 23.6 MB (85.82%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -8,20 +8,23 @@ public:
         if (grid[0][0] != 0 || grid[n-1][m-1] != 0) return -1;
         
         vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
-        queue<pair<int,int>> q;
-        q.push({0,0});
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        int cost = 0;
+        pq.push({1,{0,0}});
         dis[0][0] = 1;
 
         vector<int> erow = {-1, -1, -1, 0, 0, 1, 1, 1};
         vector<int> ecol = {-1, 0, 1, -1, 1, -1, 0, 1};
 
 
-        while(!q.empty()){
-            auto it = q.front();
-            int row = it.first;
-            int col = it.second;
+        while(!pq.empty()){
+            auto it = pq.top();
+            int row = it.second.first;
+            int col = it.second.second;
             
-            q.pop();
+            pq.pop();
+
+            if( row == n-1 && col == m-1 ) return dis[n-1][m-1];
 
             for( int i = 0 ; i < 8 ; i++ ) {
 
@@ -30,14 +33,14 @@ public:
                 if( nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 0 ){
                     if(dis[nrow][ncol] > dis[row][col] + 1){
                         dis[nrow][ncol] = dis[row][col] + 1;
-                        q.push({nrow,ncol});
+                        pq.push({dis[nrow][ncol],{nrow,ncol}});
                     }
                 }
             }
         }
 
-        if(dis[n-1][m-1] == INT_MAX) return -1;
-        return dis[n-1][m-1];
+        return -1;
+       
 
     }
 };
