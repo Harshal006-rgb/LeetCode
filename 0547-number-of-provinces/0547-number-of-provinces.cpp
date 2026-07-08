75dffdf1910a56d7c23bd6ff0aa8203423efc396
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-07-09 00:26:24 +0530

    0547-NUMBER-OF-PROVINCES Stats: Time: 10 ms (5.25%), Space: 19.6 MB (61.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,40 +1,84 @@
+#include <vector>
+using namespace std;
+
+// Using disjoit set method
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 1); // Every node initially has size 1
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    // Find ultimate parent (with Path Compression)
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    // 1. Union by Rank
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    // 2. Union by Size
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
 
-        vector<vector<int>> adj(n);
+        DisjointSet ds(n);
 
         for( int i = 0 ; i < n ; i++ ) {
             for( int j = 0 ; j < n ; j++ ) {
                 if(i==j) continue;
                 if(isConnected[i][j] == 1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                    ds.unionBySize(i,j);                 
                 }
             }
         }
 
-        vector<int> vis(n,0);
         int cnt = 0;
 
         for( int i = 0 ; i < n ; i++ ) {
-            if(!vis[i]){
-                dfs(adj,vis,i);
-                cnt++;
-            }
+            if( ds.findUPar(i) == i ) cnt++;
         }
         return cnt;
     }
-
-    void dfs( vector<vector<int>> &adj ,vector<int> &vis , int node ){
-    vis[node] = 1;
-
-    for( int edge : adj[node] ) {
-        if(!vis[edge])
-        dfs(adj,vis,edge);
-    }
-}
 
 };
 
