c7005ddf65c6e9d222efd83f455f8d5970920545
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-07-12 01:27:27 +0530

    1756-MINIMUM-DELETIONS-TO-MAKE-STRING-BALANCED Stats: Time: 48 ms (16.03%), Space: 40.5 MB (22.57%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -5,19 +5,18 @@ public:
     {
         int n = s.size();
         vector<int> aright(n,0);
-        vector<int> bleft(n,0);
+        // vector<int> bleft(n,0);
         
         for( int i = n-2 ; i >= 0 ; i-- ) {
             aright[i] = aright[i+1] + (s[i+1] == 'a');
         }
 
-        for( int i = 1 ; i < n ; i++ ) {
-            bleft[i] = bleft[i-1] + (s[i-1] == 'b');
-        }
+        int bleft = 0;
 
         int ans = INT_MAX;
         for( int i = 0 ; i < n ; i++ ) {
-            ans = min( ans , aright[i] + bleft[i] );
+            ans = min( ans , aright[i] + bleft );
+            if( s[i] == 'b' ) bleft++;
         }
 
         return ans;
