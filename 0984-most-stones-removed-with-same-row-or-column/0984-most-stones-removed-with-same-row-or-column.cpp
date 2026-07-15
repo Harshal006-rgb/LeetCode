47d1c470297abc65edc1d0cbfe0d6a4dd7c2ba69
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-07-15 18:16:13 +0530

    0984-MOST-STONES-REMOVED-WITH-SAME-ROW-OR-COLUMN Stats: Time: 43 ms (32.78%), Space: 19.5 MB (70.28%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,39 +1,75 @@
+class DisjointSet {
+ public:
+  vector<int> rank, parent, size;
+  DisjointSet(int n) {
+    rank.resize(n + 1, 0);
+    size.resize(n + 1, 1);  // Every node initially has size 1
+    parent.resize(n + 1);
+    for (int i = 0; i <= n; i++) {
+      parent[i] = i;
+    }
+  }
+
+  // Find ultimate parent (with Path Compression)
+  int findUPar(int node) {
+    if (node == parent[node]) return node;
+    return parent[node] = findUPar(parent[node]);
+  }
+
+  // 1. Union by Rank
+  void unionByRank(int u, int v) {
+    int ulp_u = findUPar(u);
+    int ulp_v = findUPar(v);
+    if (ulp_u == ulp_v) return;
+    if (rank[ulp_u] < rank[ulp_v]) {
+      parent[ulp_u] = ulp_v;
+    } else if (rank[ulp_v] < rank[ulp_u]) {
+      parent[ulp_v] = ulp_u;
+    } else {
+      parent[ulp_v] = ulp_u;
+      rank[ulp_u]++;
+    }
+  }
+
+  // 2. Union by Size
+  void unionBySize(int u, int v) {
+    int ulp_u = findUPar(u);
+    int ulp_v = findUPar(v);
+    if (ulp_u == ulp_v) return;
+
+    if (size[ulp_u] < size[ulp_v]) {
+      parent[ulp_u] = ulp_v;
+      size[ulp_v] += size[ulp_u];
+    } else {
+      parent[ulp_v] = ulp_u;
+      size[ulp_u] += size[ulp_v];
+    }
+  }
+};
 
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        vector<vector<int>> adj(n);
+        DisjointSet ds(n);
 
         for( int i = 0 ; i < n-1 ; i++ ) {
             for( int j = i+1 ; j < n ; j++ ) {
                 if( stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                    ds.unionBySize(i,j);
                 }
             }
         }
 
-        vector<int> vis(n,0);
-
-        int grp = 0;
-
+        int candelete = 0;
+        unordered_set<int> st;
         for( int i = 0 ; i < n ; i++ ) {
-            if(!vis[i]){
-                dfs(i,adj,vis);
-                grp++;
-            }
+            int parent = ds.findUPar(i);
+            if( st.find(parent) != st.end()) continue;
+            st.insert(parent);
+            candelete += ds.size[parent] - 1;
         }
 
-        return n-grp;
-    }
-
-    void dfs( int i , vector<vector<int>> &adj ,vector<int> &vis){
-        vis[i] = 1;
-        for( int idx : adj[i] ){
-            if(!vis[idx]){
-                dfs(idx,adj,vis);
-            }
-        } 
+        return candelete;
     }
 };
