5275ff9363ce15083bafd6ee56e66b03a910d158
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-07-19 21:01:37 +0530

    0686-REPEATED-STRING-MATCH Stats: Time: 0 ms (100%), Space: 11.3 MB (12.99%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,8 +1,7 @@
-class Solution
-{
+class Solution {
 public:
-    int repeatedStringMatch(string a, string b)
-    {
+
+    int repeatedStringMatch(string a, string b){
         int n = a.size();
         int m = b.size();
         int cnt = m/n;
@@ -15,11 +14,65 @@ public:
             i++;
         }
 
-        if( na.find(b) != std::string::npos) return cnt;
+        if(strStr(na,b)) return cnt;
         na += a;
-        if( na.find(b) != std::string::npos) return cnt+1;
+        if(strStr(na,b)) return cnt+1;
         na += a;
-        if( na.find(b) != std::string::npos) return cnt+2;
+        if(strStr(na,b)) return cnt+2;
         return -1;
+    }
+
+    bool strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+
+        int i = 0;
+        int j = 0;
+        vector<int> lps = getlps(needle);
+
+        while(i<n){
+            if( haystack[i] == needle[j]){
+                i++; j++;
+            }
+
+            if( j == m ){
+                return true;
+            }
+            else if ( i<n && haystack[i] != needle[j]) {
+                if(j!=0){
+                    j = lps[j-1];
+                }
+                else i++;
+            }
+
+        }
+
+        return false;
+    }
+    
+    vector<int> getlps( string s ){
+        int n = s.size();
+        vector<int> lps(n);
+        lps[0] = 0;
+        int i = 1;
+        int len = 0;
+
+        while(i<n){
+            if( s[i] == s[len]){
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else{
+                if(len != 0){
+                    len = lps[len-1];
+                }
+                else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
     }
 };
