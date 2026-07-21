97225a10900337c952bd2a31bb358c93f9a8bbbf
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-07-21 20:47:35 +0530

    1746-LARGEST-SUBSTRING-BETWEEN-TWO-EQUAL-CHARACTERS Stats: Time: 0 ms (100%), Space: 8.9 MB (28.14%) - LeetHub

--- 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cpp
@@ -2,29 +2,19 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        int n = s.size();
-        int l = 0 , r = n-1;
-        unordered_map<char,int> smp,emp;
-
-        for( int i = 0 ; i < n ; i++ ) {
-            if( smp.find(s[i]) == smp.end()){
-                smp[s[i]] = i;
-            }
-        }
-
-        for( int i = n-1 ; i >= 0 ; i-- ) {
-            if( emp.find(s[i]) == emp.end()){
-                emp[s[i]] = i;
-            }
-        }
-
-        int size = -1;
-
-        for( int i = 0 ; i < n ; i++ ) {
-            size = max( size , emp[s[i]] - smp[s[i]] -1 );   
-        }
+        unordered_map<char, int> firstIndex;
+        int ans = -1;
         
-        if(size<0) return -1;
-        return size;
+        for (int i = 0; i < s.size(); i++) {
+            if (firstIndex.find(s[i]) != firstIndex.end()) {
+                ans = max(ans, i - firstIndex[s[i]] - 1);
+            } else {
+                firstIndex[s[i]] = i;
+            }
+        }
+
+        return ans;
     }
 };
+
+
