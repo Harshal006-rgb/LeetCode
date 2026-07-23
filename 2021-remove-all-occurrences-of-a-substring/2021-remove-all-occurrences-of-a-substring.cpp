00bc616e02f8496a117d02890134e02f790831c3
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-07-23 19:46:43 +0530

    2021-REMOVE-ALL-OCCURRENCES-OF-A-SUBSTRING Stats: Time: 2 ms (41.74%), Space: 9.4 MB (42.33%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        string ans;
-        int n = s.size();
-        int m = part.size();
-
-        for( int i = 0 ; i < n ; i++ ) {
-            ans+= s[i];
-            if( ans.size() >= m && s[i] == part.back() ){
-                if(ans.substr(ans.size()-m,m) == part ){
-                    for( int i = 0 ; i < m ; i++ ) ans.pop_back();
-                }
-            }
+        while (s.find(part) != string::npos) {
+            s.erase(s.find(part), part.size());
         }
-        return ans;
+        return s;
     }
 };
