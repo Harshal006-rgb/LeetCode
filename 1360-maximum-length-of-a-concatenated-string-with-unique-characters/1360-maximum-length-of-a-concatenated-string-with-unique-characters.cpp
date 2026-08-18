b9d1f3bec21da181fbe02f9ec4369d4c5eab1d25
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-08-19 00:15:18 +0530

    1360-MAXIMUM-LENGTH-OF-A-CONCATENATED-STRING-WITH-UNIQUE-CHARACTERS Stats: Time: 86 ms (31.21%), Space: 75.9 MB (27.12%) - LeetHub

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,7 +1,10 @@
-
 class Solution {
 public:
+
+    unordered_map<string,int> mp;
+
     int maxLength(vector<string>& arr) {
+        mp.empty();
         int n = arr.size();
         string temp;
         int i = 0;
@@ -10,22 +13,21 @@ public:
 
     bool uniqueChar( string s1 , string s2 ){
         vector<int> hash(26,0);
-
         for( char c : s1 ){
             if( hash[c-'a'] > 0 ) return false;
             hash[c-'a']++;
         }
-
         for( char c : s2 ){
             if( hash[c-'a'] > 0 ) return false;
             hash[c-'a']++;
         }
-
         return true;
     }
 
     int rec( int i , string temp , vector<string> &arr, int n ){
         if( i == n ) return temp.size();
+
+        if( mp.find(temp) != mp.end()) return mp[temp];
 
         int include = 0;
         int exclude = 0;
@@ -38,6 +40,6 @@ public:
             exclude = rec(i+1,temp,arr,n);
         }
 
-        return max(exclude,include);
+        return mp[temp] = max(exclude,include);
     }
 };
