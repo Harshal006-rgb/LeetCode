fb583a2636bf1e1f5a38f65129be792ef59a4390
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-08-21 00:15:05 +0530

    2888-MINIMUM-INDEX-OF-A-VALID-SPLIT Stats: Time: 0 ms (100%), Space: 90 MB (70.9%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -1,27 +1,44 @@
-class Solution {
-public:
-    int minimumIndex(vector<int>& nums) {
-        int n = nums.size();
+    class Solution {
+    public:
+        int minimumIndex(vector<int>& nums) {
+            int n = nums.size();
 
-        unordered_map<int,int> mp1;
-        unordered_map<int,int> mp2;
+            int number;
+            int times = 0;
 
-        for( int num : nums ) mp1[num]++;
-
-        for( int i = 0 ; i < n ; i++ ) {
-        
-            int num = nums[i];
-            mp1[num]--;
-            mp2[num]++;
-
-            int n1 = i+1;
-            int n2 = n-1-i;
-
-            if( mp1[num]*2 >n2 && mp2[num]*2 >n1 ){
-                return i;
+            for( int i = 0 ; i < n ; i++ ) {
+                if( times == 0 ){
+                    times = 1;
+                    number = nums[i];
+                }
+                else if ( number == nums[i]) times++;
+                else times--;
             }
 
+            if( times == 0 ) return -1;
+
+            times = 0;
+
+            for( int i = 0 ; i < n ; i++ ) {
+                if( number == nums[i] ) times++;
+            }
+
+            int count = 0;
+
+            for( int i = 0 ; i < n-1 ; i++ ) {
+            
+                if( number == nums[i]) count++;
+
+                int n1 = i+1;
+                int n2 = n-1-i;
+
+                if( count*2 >n1 && (times-count)*2 >n2 ){
+                    return i;
+                }
+            }
+            return -1;
         }
-        return -1;
-    }
-};
+    };
+
+
+
