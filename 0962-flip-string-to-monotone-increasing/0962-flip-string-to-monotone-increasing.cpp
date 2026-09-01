d9dc95ab48099ba5987200a6989502a279c80bcf
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-09-01 17:12:36 +0530

    0962-FLIP-STRING-TO-MONOTONE-INCREASING Stats: Time: 215 ms (17.37%), Space: 90.8 MB (18.72%) - LeetHub

--- 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.cpp
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
+    int n = 0;
+
     int minFlipsMonoIncr(string s) {
-        int n = s.size();
-        int zerocnt = 0;
-        int onecnt = 0;
-        for( int i = 0 ; i < n ; i++ ) {
-            if( s[i] -'0' == 0 ) zerocnt++;
+        n = s.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        return solve( s,0,0,dp );
+    }
+
+
+    int solve(string &s , int i , int prev , vector<vector<int>> &dp ){
+        if( i == n ) return 0;
+
+        if( dp[i][prev] != -1 ) return dp[i][prev];
+
+        int flip = INT_MAX;
+        int nonflip = INT_MAX;
+
+        if( s[i] == '0' ){
+            if( prev == 0 ){
+                nonflip = solve(s,i+1,0,dp);
+            }
+            flip = 1+solve(s,i+1,1,dp);
         }
-        int ans = zerocnt;
-
-        for( int i = 0 ; i < n ; i++ ) {
-            if( s[i]-'0' == 0 ) zerocnt--;
-            else onecnt++;
-            ans = min(ans,zerocnt+onecnt);
+        else{
+            if( prev == 0 ){
+                flip = 1+solve(s,i+1,0,dp);
+            }
+            nonflip = solve(s,i+1,1,dp);
         }
 
-        return ans;
-
+        return dp[i][prev] = min(flip,nonflip);
     }
 };
