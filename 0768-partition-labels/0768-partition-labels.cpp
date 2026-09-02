50e04de9d4bbd8b62e28b79f4172b3569028f632
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-09-02 16:38:06 +0530

    0768-PARTITION-LABELS Stats: Time: 2 ms (32.47%), Space: 9 MB (20.63%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         int n = s.size();
-        vector<int> last_pos(26, 0);
-        // 1. Store the last occurrence index of each character
-        for (int i = 0; i < n; i++) {
-            last_pos[s[i] - 'a'] = i;
+        vector<int> last_pos(26,-1);
+
+        for( int i = 0 ; i < n ; i++ ) {
+            last_pos[s[i]-'a'] = i;
         }
+
+        int start = 0;
+        int end = 0;
         vector<int> result;
-        int start_idx = 0;
-        int max_last = 0;
-        // 2. Greedy partition split
-        for (int i = 0; i < n; i++) {
-            max_last = max(max_last, last_pos[s[i] - 'a']);
-            
-            // If we reach the furthest index of all characters in current partition
-            if (i == max_last) {
-                result.push_back(i - start_idx + 1);
-                start_idx = i + 1; // Move starting point to the next block
+
+        for( int curr = 0 ; curr < n ; curr++ ) {
+            if( curr > end ){
+                result.push_back(end-start+1);
+                start = curr;
             }
+            end = max(end,last_pos[s[curr]-'a']);
         }
+
+        result.push_back(end-start+1);
         return result;
     }
 };
